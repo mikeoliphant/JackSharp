1426d8829d373a2a568609e7e7b2d7979bd4c315
Emanuel Knecht <h3ll5ur7er@gmail.com>
2024-01-04 13:23:39 +0100

    add audio transcription project to solution

---
+++ AudioTranscription/Program.cs
@@ -0,0 +1,40 @@
+﻿using Jack.NAudio;
+using JackSharp;
+using NAudio;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+
+System.Console.WriteLine("Recording");
+
+using var client = new Processor("AudioTranscription", 2, 2, 0, 0, true);
+client.Start();
+Console.ReadKey();
+using var audioIn = new AudioIn(client);
+System.Console.WriteLine(audioIn.WaveFormat);
+using var audioOut = new AudioOut(client);
+using var stream = new MemoryStream();
+
+audioIn.DataAvailable += (s,e) => {
+    stream.Write(e.Buffer, 0, e.BytesRecorded);
+};
+audioIn.RecordingStopped += (s,e) => {
+    Console.WriteLine("Recording Stopped");
+    stream.Position = 0;
+};
+audioIn.StartRecording();
+Thread.Sleep(TimeSpan.FromSeconds(5));
+audioIn.StopRecording();
+Thread.Sleep(TimeSpan.FromSeconds(1));
+
+///////////////////////////////////////////////
+
+System.Console.WriteLine("Playback");
+
+var wavStream = new RawSourceWaveStream(stream, audioIn.WaveFormat);
+var resampled = new WdlResamplingSampleProvider(wavStream.ToSampleProvider(), 16000);
+audioOut.Init(resampled);
+audioOut.Play();
+Thread.Sleep(TimeSpan.FromSeconds(10));
+audioOut.Stop();
+
+
